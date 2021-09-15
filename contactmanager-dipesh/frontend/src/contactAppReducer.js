faaa030072a53788fbdfeba102acdf9a0570a1e7
nguyenminhthong2020 <1612674@student.hcmus.edu.vn>
2021-09-15 15:55:38 +0700

    react-draft-wysiwyg

--- contactmanager-dipesh/frontend/src/contactAppReducer.js
+++ contactmanager-dipesh/frontend/src/contactAppReducer.js
@@ -10,45 +10,58 @@ export default function reducer(state, action) {
         cards: [...state.cards, { ...action.payload.new_card, id: state.cards.length, isExist: true }]
       }
 
-    case 'like': {
-      const obj = JSON.parse(localStorage.getItem("contactManagerApp_cards"))
-      console.log(obj[action.payload.id].like);
-      // let bien = localStorage.getItem("contactManagerApp_cards");
-      // let bien1 = JSON.parse(bien);
-      // let bien2 = bien1[action.payload.id];
-      // let valueLike = bien2.like;
-      if ((obj[action.payload.id].like) === false) {
-        obj[action.payload.id].like = !(obj[action.payload.id].like);
-        const str = JSON.stringify(obj);
-        localStorage.setItem("contactManagerApp_cards", str);
-        // console.log(obj[action.payload.id].like);
-        return {
-          cards: state.cards.map(c => c.id === action.payload.id ? { ...c, like: c.like + 1 } : c)
-        }
-      } 
-      if((obj[action.payload.id].like) === true){
-        obj[action.payload.id].like = !(obj[action.payload.id].like);
-        const str = JSON.stringify(obj);
-        localStorage.setItem("contactManagerApp_cards", str);
-        return {
-          cards: state.cards.map(c => c.id === action.payload.id ? { ...c, like: c.like - 1 } : c)
-        }
-      }
-    }
-
-    case 'dislike':
-      // const obj = JSON.parse(localStorage.getItem("contactManagerApp_cards"))
-      // obj[id].dislike = !obj[id].dislike;
-      // const str = JSON.stringify(obj);
-      // localStorage.setItem("contactManagerApp_cards", str);
-      return {
-        cards: state.cards.map(c => c.id === action.payload.id ? { ...c, dislike: c.dislike + 1 } : c)
-      }
-
     case 'delete_card':
       return {
         cards: state.cards.map(c => c.id === action.payload.id ? { ...c, isExist: false } : c)
       }
+
+    case 'increase_like':
+      return {
+        cards: state.cards.map(c => c.id === action.payload.id ? { ...c, like: c.like + 1 } : c)
+      }
+
+    case 'decrease_like':
+      return {
+        cards: state.cards.map(c => c.id === action.payload.id ? { ...c, like: c.like - 1 } : c)
+      }
+
+    case 'increase_dislike':
+      return {
+        cards: state.cards.map(c => c.id === action.payload.id ? { ...c, dislike: c.dislike + 1 } : c)
+      }
+
+    case 'decrease_dislike':
+      return {
+        cards: state.cards.map(c => c.id === action.payload.id ? { ...c, dislike: c.dislike - 1 } : c)
+      }
+
+    // case 'like': {
+    //   const obj = JSON.parse(localStorage.getItem("contactManagerApp_cards"))
+    //   //console.log(obj[action.payload.id].like);
+
+    //   // let bien = localStorage.getItem("contactManagerApp_cards");
+    //   // let bien1 = JSON.parse(bien);
+    //   // let bien2 = bien1[action.payload.id];
+    //   // let valueLike = bien2.like;
+    //   if ((obj[action.payload.id].like) === false) {
+    //     obj[action.payload.id].like = !(obj[action.payload.id].like);
+    //     const str = JSON.stringify(obj);
+    //     localStorage.setItem("contactManagerApp_cards", str);
+    //     // console.log(obj[action.payload.id].like);
+    //     return {
+    //       cards: state.cards.map(c => c.id === action.payload.id ? { ...c, like: c.like + 1 } : c)
+    //     }
+    //   } 
+    //   if((obj[action.payload.id].like) === true){
+    //     obj[action.payload.id].like = !(obj[action.payload.id].like);
+    //     const str = JSON.stringify(obj);
+    //     localStorage.setItem("contactManagerApp_cards", str);
+    //     return {
+    //       cards: state.cards.map(c => c.id === action.payload.id ? { ...c, like: c.like - 1 } : c)
+    //     }
+    //   }
+    // }
+
 
     case 'update_filter':
       return {
